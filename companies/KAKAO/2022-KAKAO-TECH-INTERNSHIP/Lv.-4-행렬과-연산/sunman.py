8c9b2982b1479111670cd975a7f9b4fc737d3c13
sunman.kim <sunman.kim@navercorp.com>
2022-09-16 00:57:17 +0900

    행렬과 연산.. 틀렸다..

--- companies/KAKAO/2022-KAKAO-TECH-INTERNSHIP/Lv.-4-행렬과-연산/sunman.py
+++ companies/KAKAO/2022-KAKAO-TECH-INTERNSHIP/Lv.-4-행렬과-연산/sunman.py
@@ -4,68 +4,58 @@ operations_count = {'ShiftRow': 0, 'Rotate': 0}
 rcs = []
 repetition_count = 0
 
-first_row = [''] * column_count
-right_column = [''] * (row_count - 2)
-last_row_reverse = [''] * column_count
-left_column_reverse = [''] * (row_count - 2)
-
-
 def findCoord(count):
     global repetition_count
     pure_count = count % repetition_count
-    # first_row에 넣는다
+    # 맨 윗줄
     if 0 <= pure_count < column_count:
         return [pure_count, 0]
-    # right_column에 넣는다
-    elif column_count <= pure_count < column_count + (row_count - 2):
-        return [column_count - 1, pure_count - column_count + 1]
-    # last_row_reverse에 넣는다
-    elif column_count + (row_count - 2) <= pure_count < column_count * 2 + (row_count - 2):
-        return [pure_count - column_count - row_count + 2, row_count - 1]
-    # left_column_reverse에 넣는다
+    # 맨 오른줄
+    elif column_count <= pure_count < column_count + (row_count - 1):
+        return [column_count - 1, pure_count - (column_count - 1)]
+    # 맨 아래줄
+    elif column_count + (row_count - 1) <= pure_count < column_count * 2 + (row_count - 2):
+        return [column_count - 1 - (pure_count - (column_count - 1 + row_count - 1)), row_count - 1]
+    # 맨 왼줄
     else:  # column_count*2 + (row_count - 2)*2
-        return [0, pure_count - column_count - row_count - count + 3]
+        return [0, row_count - 1 - (pure_count - (column_count - 1 + row_count - 1 + column_count - 1))]
 
 
 def makeRotate(count):
-    global rcs, repetition_count, first_row, right_column, last_row_reverse, left_column_reverse
+    global rcs, repetition_count
     pure_count = count % repetition_count
-    first_row = [''] * column_count
-    right_column = [''] * (row_count - 2)
-    last_row_reverse = [''] * column_count
-    left_column_reverse = [''] * (row_count - 2)
+    temp_copy_stack = []
+
     for index in range(repetition_count):
+        # [0,0] 부터 시작하여 시계방향으로 원소를 뽑아 stack에 넣는다
+        [x, y] = findCoord(index)
+        temp_copy_stack.append(rcs[y][x])
+
+    for index in range(repetition_count):
+        # 뽑아 만든 stack을 순서대로 추출하여 n 만큼 회전한 곳에 붙여넣는다.
         target_index = pure_count + index
-
-    # 첫번째와 마지막 row를 직접 만든다
-    # 중간 row들은 맨 앞 뒤 element만 변경한다
-    # 이후 rcs의 첫번째와 마지막 row를 교체한다 얕은 복사로 넣는다!
-
-    # 교체 후 초기화
-    first_row = [''] * column_count
-    right_column = [''] * (row_count - 2)
-    last_row_reverse = [''] * column_count
-    left_column_reverse = [''] * (row_count - 2)
+        [x, y] = findCoord(target_index)
+        rcs[y][x] = temp_copy_stack[index]
 
 
 def makeShiftRow(count):
     global rcs
     pure_count = count % row_count
-    rcs = rcs[-pure_count:] + rcs[:pure_count - 1]
+    if (pure_count):
+        rcs = rcs[-pure_count:] + rcs[:row_count - (pure_count)]
 
 
 def solution(rc, operations):
     global row_count, column_count, operations_count, rcs, repetition_count
     rcs = rc
-    row_count = len(rcs)  # 전역 변수 변경
-    column_count = len(rcs[0])  # 전역 변수 변경
+    row_count = len(rcs)
+    column_count = len(rcs[0])
     repetition_count = row_count * 2 + column_count * 2 - 4
     last_operation = "ShiftRow"
     for index in range(len(operations)):
         current_operation = operations[index]
         if last_operation == current_operation:
             operations_count[current_operation] += 1
-            pass
         else:
             # 다르다면 이전까지 있었던 operation을 그 횟수에 맞게 먼저 수행
             if last_operation == "ShiftRow":
@@ -78,9 +68,25 @@ def solution(rc, operations):
             operations_count[current_operation] = 1
             # 새 operation을 기록
             last_operation = current_operation
-    # 남은 operation 처리하기
-    if last_operation == "ShiftRow":
-        makeShiftRow(operations_count[last_operation])
-    else:
+
+    # 혹시나 잔여 operation이 있다면 처리
+    if (operations_count['Rotate']):
         makeRotate(operations_count[last_operation])
+    if (operations_count['ShiftRow']):
+        makeShiftRow(operations_count[last_operation])
     return rcs
+
+
+# print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ["Rotate", "ShiftRow"]))
+# print('------------ real anwer ------------')
+# print([[8, 9, 6], [4, 1, 2], [7, 5, 3]])
+
+
+# print(solution([[8, 6, 3], [3, 3, 7], [8, 4, 9]],["Rotate", "ShiftRow", "ShiftRow"]))
+# print('------------ real anwer ------------')
+# print([[8, 3, 3], [4, 9, 7], [3, 8, 6]])
+
+
+# print(solution([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], ["ShiftRow", "Rotate", "ShiftRow", "Rotate"]))
+# print('------------ real anwer ------------')
+# print([[1, 6, 7 ,8], [5, 9, 10, 4], [2, 3, 12, 11]])
